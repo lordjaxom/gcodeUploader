b43ac677b2101474f7cefd3897fbb47f1f970d73
Sascha Volkenandt <sascha@akv-soft.de>
2018-03-19 11:39:22 +0100

    cleaned unused library functions

---
+++ CMakeLocal.PC10005801.cmake
@@ -0,0 +1,5 @@
+set(BOOST_ROOT "C:/Users/VolkenaS/Programme/boost_1_66_0")
+set(JSON_ROOT "C:/Users/VolkenaS/Programme/json-master")
+set(wxWidgets_ROOT_DIR "C:/Users/VolkenaS/Programme/wxWidgets-3.1.0")
+set(LIB3DPRNET_ROOT "E:/Projekte/CPP/lib3dprnet")
+set(LIB3DPRNET_BUILD_SUFFIX "")
